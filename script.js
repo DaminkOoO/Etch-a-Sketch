9802e2287c3a8a4a7b96f5c9e8cd39da34dae248
daminko <kessntiniamin@gmail.com>
2024-08-26 12:03:42 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 
 let getColor = () => document.querySelector("#colorInput").value;
-function getContainer() {
-    return document.querySelector(".container");
+let container = document.querySelector(".container");
+let slider = document.querySelector("#myRange");
+let sliderSpan = document.querySelector(".sliderValue");
+const resetButton = document.querySelector("#reset");
+let isClicking = false;
+container.onmousedown = () => {
+
+    isClicking = true;
 }
+container.onmouseup = () => {
+    isClicking = false;
+
+}
+function render() {
+    sliderSpan.textContent = `${slider.value} x ${slider.value}`;
+    clearGrid(container);
+    generateDivs(slider.value, container);
+}
+
 function generateDivs(numberOfDivs, container) {
     let boxSize = (container.offsetWidth) / numberOfDivs;
-    console.log(boxSize);
     let listOfDivs = [];
-    for (let i = 0; i < numberOfDivs; i++) {
-        for (let j = 0; j < numberOfDivs; j++) {
-            let div = document.createElement("div");
-            div.setAttribute("id", i);
-            div.setAttribute("class", 'box');
-            div.textContent = j
-            div.onmouseover = () => {
+    numberOfDivs = numberOfDivs * numberOfDivs;
+    for (let j = 0; j < numberOfDivs; j++) {
+        let div = document.createElement("div");
+        div.setAttribute("id", j);
+        div.setAttribute("class", 'box');
+        div.setAttribute("draggable", 'false');
+        div.onmouseover = () => {
+            if (isClicking)
                 div.style.backgroundColor = getColor();
-            }
-            div.setAttribute("style", `width:${boxSize}px;height:${boxSize}px`)
-            container.appendChild(div);
         }
-
+        div.setAttribute("style",
+            `width:${boxSize}px;height:${boxSize}px`)
+        container.appendChild(div);
     }
+
     return listOfDivs;
 }
-generateDivs(16, getContainer());
-
-
-
-
+function clearGrid(container) {
+    container.innerHTML = '';
+}
+function resetGrid(container) {
+    console.log("rest");
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = 'white';
+    });
+}
 
